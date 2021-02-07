0cfd555c48789d73b26dd4d911d5b601457f8d79
Muhun Kim <iam@muhun.kim>
2021-02-07 10:44:18 +0900

    Implement iteration version

--- src/1-12.py
+++ src/1-12.py
@@ -16,3 +16,27 @@ def test_pascal():
     assert pascal_row(3) == [1, 2, 1]
     assert pascal_row(4) == [1, 3, 3, 1]
     assert pascal_row(5) == [1, 4, 6, 4, 1]
+
+
+def pascal_iter(n: int):
+    reducing = []
+
+    def pascal(row: int, col: int):
+        if (row == 0):
+            return 1
+        if (col == 0 or row == col):
+            return 1
+        return reducing[row - 1][col - 1] + reducing[row - 1][col]
+
+    for row in range(n):
+        reducing.append(
+            list(map(lambda col: pascal(row, col), range(row+1))))  # +1 is offset by range(n)
+
+    return reducing
+
+
+def test_pascal_iter():
+    assert pascal_iter(4) == [[1],
+                              [1, 1],
+                              [1, 2, 1],
+                              [1, 3, 3, 1]]

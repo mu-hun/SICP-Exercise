aff95a6fe7cb240ed12ae66c4590c1baf924f4ed
Muhun Kim <iam@muhun.kim>
2021-03-12 15:01:18 +0900

    Rename to sqrt-cube

--- src/1-41.test.rkt
+++ src/1-41.test.rkt
@@ -5,8 +5,8 @@
 
 (test-case "Define a procedure double that takes a procedure of one argument as argument and returns a procedure that applies the original procedure twice."
 
-  (define sqrt-2 (double sqrt))
+  (define sqrt-cube (double sqrt))
 
-  (check-equal? (sqrt-2 16) 2)
+  (check-equal? (sqrt-cube 16) 2)
   (check-equal? (((double (double double)) inc) 5) 21)
 )
